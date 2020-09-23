fe1b3bb2068e98e3bc82ff1dcb8448108ec4e3fb
Yoondy <ydh0213@gmail.com>
2020-09-24 01:17:01 +0900

    첫 빌드 전체 파일 업로드. ActionBar 나오지 않도록 수정.

---
+++ app/src/main/java/com/makeus/wisdomexchange/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.makeus.wisdomexchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
