78a6ddf918d144af1682ca4c01875e6515104b00
Yoondy <ydh0213@gmail.com>
2020-10-26 07:55:18 +0900

    더미데이터 더 현실적으로 수정

--- app/src/main/java/com/makeus/wisdomexchange/src/main/feed/FeedFragment.kt
+++ app/src/main/java/com/makeus/wisdomexchange/src/main/feed/FeedFragment.kt
@@ -38,7 +38,7 @@ class FeedFragment : Fragment() {
         feedRvAdapter.add(Feed(1, "익명", "Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt", "오전 12:00", null, 0, 0))
         feedRvAdapter.add(Feed(2, "운영자", "피드 테스트", "오전 4:15", "https://i.pinimg.com/originals/76/47/9d/76479dd91dc55c2768ddccfc30a4fbf5.png", 0, 0))
         feedRvAdapter.add(Feed(3, "운영자", "공지사항\n서버 점검이 있을 예정이므로 양해바랍니다.", "오전 4:17", "", 2, 0))
-        feedRvAdapter.add(Feed(4, "익명", "Lorem ipsum d   olor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt", "오전 12:00", null, 0, 0))
+        feedRvAdapter.add(Feed(4, "가짜개발자", "알고리즘 어려웡ㅇㅇ", "오전 12:00", null, 0, 0))
         feedRvAdapter.add(Feed(5, "익명", "Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt", "오전 12:00", null, 0, 0))
         feedRvAdapter.add(Feed(6, "익명", "혹시 이거 아는 분 계세요?? 친구한테 받은건데 뭔지 모르겠어요", "오후 1:39", "https://image.fmkorea.com/files/attach/new/20180825/486616/72458075/1231630583/43e2b25138ce2397c43dca8b6a0fa40f.jpg", 17, 3))
         feedRvAdapter.add(Feed(7, "패피", "OOTD 올려봅니다", "오후 4:05", "https://i.pinimg.com/736x/52/3a/92/523a92bccc5e534c343a986f5b0f7878.jpg", 127, 16))
@@ -47,10 +47,10 @@ class FeedFragment : Fragment() {
         feedRvAdapter.add(Feed(10, "윤디", "가을이 온다... 흑흑 외로웡ㅇㅇ유ㅠㅠㅠㅠ", "오후 7:27", "https://dimg.donga.com/wps/NEWS/IMAGE/2013/11/11/58808007.3.jpg",   54, 35))
         feedRvAdapter.add(Feed(11, "???", "이걸 보는 자 다들 올해도 솔크를 보낼터이니 크킄ㅋ크ㅡ크킄나만당할순없지 전부 저주에 걸려버려라 아ㅏㅏ앙앙ㅇ아ㅏ아 솔로천국커플지옥 다 망해버려라ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ", "오후 7:29", "https://jjalbang.today/files/jjalbox/2015/03/102_5516a42c36655_933.jpg", 87, 5))
         feedRvAdapter.add(Feed(12, "록", "아아아니 이거 실화냐", "오후 9:58", null, 60, 7))
-        feedRvAdapter.add(Feed(13, "익명", "Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt", "오전 12:00", "http://ram.komica2.net/00/src/1601988501524.jpg", 0, 0))
-        feedRvAdapter.add(Feed(14, "익명", "Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt",  "오전 12:00", "http://ram.komica2.net/00/src/1601988443862.jpg", 0, 0))
+        feedRvAdapter.add(Feed(13, "아이마스", "왜 나는 얘네들 사이에서...", "오전 12:00", "http://ram.komica2.net/00/src/1601988501524.jpg", 0, 0))
+        feedRvAdapter.add(Feed(14, "아이마스", "왜 나는 얘네들 사이에서... (2)",  "오전 12:00", "http://ram.komica2.net/00/src/1601988443862.jpg", 0, 0))
         feedRvAdapter.add(Feed(15, "익명", "Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt", "오전 12:00", "", 0, 0))
-        feedRvAdapter.add(Feed(16, "익명", "Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt Lorem ipsum dolor sit amet, consectetur adipis elit, sed do eiusmod tempor incididunt", "오전 12:00", "https://post-phinf.pstatic.net/MjAyMDA4MjVfMTY1/MDAxNTk4MzUyMzg5MDk5.uSx55RCqy1yzbFcvGVTdbA1ZWoeZGeMpVMyuvyVDIEYg.h36_s8YhaD4tKTma30yTw6e_QoP4SKJ52C1OZeGcB2Eg.JPEG/IMG_5471.jpg", 0, 0))
+        feedRvAdapter.add(Feed(16, "페이몬", "원신 요새 넘 재밌네요 원신 하는 분 계신가요", "오전 12:00", "https://post-phinf.pstatic.net/MjAyMDA4MjVfMTY1/MDAxNTk4MzUyMzg5MDk5.uSx55RCqy1yzbFcvGVTdbA1ZWoeZGeMpVMyuvyVDIEYg.h36_s8YhaD4tKTma30yTw6e_QoP4SKJ52C1OZeGcB2Eg.JPEG/IMG_5471.jpg", 0, 0))
     }
 
     companion object {
--- app/src/main/java/com/makeus/wisdomexchange/src/main/home/HomeFragment.kt
+++ app/src/main/java/com/makeus/wisdomexchange/src/main/home/HomeFragment.kt
@@ -47,22 +47,22 @@ class HomeFragment : Fragment() {
         }
 
         // TODO: 테스트 코드. 나중에 삭제해야 함
-        wisdomExchangeRvAdapter.add(WisdomExchange("", "슬기 교환서 제목", "프로그래밍", "아트",
+        wisdomExchangeRvAdapter.add(WisdomExchange("", "프로그래밍 ↔ 아트 교환", "프로그래밍", "아트",
             arrayListOf("C++", "Python"), arrayListOf("디자인", "영상"), 4.0, 123, 123))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/58e6sXz.jpg", "슬기 제목0", "내 슬기들0", "희망 슬기들0",
-                arrayListOf(), arrayListOf(), 0.0, 0, 0))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/RpMDsuF.jpg", "슬기 제목1", "내 슬기들1", "희망 슬기들1",
-                arrayListOf("내 슬기"), arrayListOf(), 1.1, 1, 1999))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/q3ZKiTw.jpg", "슬기 제목2", "내 슬기들2", "희망 슬기들2",
-            arrayListOf(), arrayListOf("희망슬기1", "희망슬기2"), 4.4, 99999, 9))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/WMrDqWz.jpg", "슬기 제목3", "내 슬기들3", "희망 슬기들3",
-            arrayListOf("내 슬기1", "내 슬기2"), arrayListOf("희망슬기"), 3.56, 413, 53))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/SIquQS2.jpg", "슬기 제목4", "내 슬기들4", "희망 슬기들4",
-            arrayListOf(), arrayListOf("희망슬기1234"), 2.7, 10, 2))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/lRDCzkG.jpg", "슬기 제목5", "내 슬기들5", "희망 슬기들5",
-            arrayListOf("내 슬기"), arrayListOf("희망 슬기"), 5.0, 5, 5))
-        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/BWNpfGp.jpg", "슬기 제목6", "내 슬기들6", "희망 슬기들6",
-                arrayListOf(), arrayListOf(), 1.8, 3576, 444))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://upload2.inven.co.kr/upload/2018/04/15/bbs/i16501873220.jpg", "옵치 랭커입니다 피아노 배워보고 싶어요", "게임", "악기",
+                arrayListOf("오버워치", "리듬게임"), arrayListOf("피아노"), 0.0, 0, 0))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://i.ytimg.com/vi/MQ0xicHaWOQ/hqdefault.jpg", "음치탈출하실 분?", "음악", "기타",
+                arrayListOf("보컬 트레이닝", "댄스"), arrayListOf("아무거나"), 1.1, 1, 1999))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQm5T1-ikKmhpXojdlzKhUZMXHPQo2lVQr2_Q", "암기법 전수해드립니다", "자기계발", "기타",
+            arrayListOf("암기법"), arrayListOf("자기관리", "패션"), 4.4, 99999, 9))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://assets.2ser.com/wp-content/aws/uploads/2018/02/21032555/411713-magician.jpg", "마술 배워보고 싶어요", "운동", "취미",
+            arrayListOf("헬스 PT"), arrayListOf("마술"), 3.56, 413, 53))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://handscoffee.com/data/cheditor4/1609/65ed35f3ff808f1b31ff70b19b19d484_AzACOXWdIhkrhAZKS6EH39.JPG", "요리 슬기 교환할 분 찾아여ㅠㅠㅠ", "라이프스타일", "라이프스타일",
+            arrayListOf("양식", "제과제빵", "바리스타"), arrayListOf("한식", "일식", "중식"), 2.7, 10, 2))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://www.forbes.fr/wp-content/uploads/2017/08/0731_lb-dj-marshmello_1200x630.jpg", "글쓰기 가르쳐주세오", "자기계발", "음악",
+            arrayListOf("글쓰기"), arrayListOf("작곡", "DJ"), 5.0, 5, 5))
+        wisdomExchangeRvAdapter.add(WisdomExchange("https://cdn.crowdpic.net/detail-thumb/thumb_d_B12DECD6BBDF50F0FC537798397B4F5E.jpg", "펜돌리기 고수 찾습니다 캘리그라피 가르쳐드려요!", "아트", "취미",
+                arrayListOf("캘리그라피"), arrayListOf("펜돌리기"), 1.8, 3576, 444))
         wisdomExchangeRvAdapter.add(WisdomExchange("", "드로잉 가르쳐드릴테니 코딩 가르쳐주실 분 구합니다", "디자인", "프로그래밍",
             arrayListOf("드로잉", "스케치", "인물구도"), arrayListOf("코딩 입문", "웹", "앱"), 3.8, 107, 19))
         wisdomExchangeRvAdapter.add(WisdomExchange("https://i.imgur.com/KodkXuB.jpeg", "영어 잘하시는 분ㅠㅠ", "외국어", "외국어",
